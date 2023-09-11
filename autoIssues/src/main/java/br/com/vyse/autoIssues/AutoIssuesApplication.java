dc8dcd65c6931ccfa0cd303c685406751f1e1c39
Gabriel Frederico Barbosa <biel1801fredy@gmail.com>
2023-09-11 17:28:11 -0300

    project

---
+++ autoIssues/src/main/java/br/com/vyse/autoIssues/AutoIssuesApplication.java
@@ -0,0 +1,13 @@
+package br.com.vyse.autoIssues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoIssuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoIssuesApplication.class, args);
+	}
+
+}
